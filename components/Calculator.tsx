cd8c54329453d1def80dabd69f7c1fabd3bf023d
Javier <javierneri81@gmail.com>
2022-10-09 03:26:54 +0800

    feature: create bare agar calculator
    Add input for agar recipe and volume.
    
    Add read only input for showing needed mass of agar for give recipe and volume.
    
    Make calculation for mass just the sum of recipe and volume as placeholder.

---
+++ components/Calculator.tsx
@@ -0,0 +1,70 @@
+import { ChangeEvent, ChangeEventHandler, useEffect, useState } from "react";
+
+const Calculator = () => {
+  const [recipeValue, setRecipeValue] = useState("0");
+  const [volumeValue, setVolumeValue] = useState("0");
+  const [massValue, setMassValue] = useState("0");
+
+  const handleRecipeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setRecipeValue(e.target.value);
+  };
+
+  const handleVolumeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setVolumeValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const conectration = parseFloat(recipeValue) || 0;
+    const finalvol = parseFloat(volumeValue) || 0;
+
+    const getMassValue = () => {
+      setMassValue((conectration + finalvol).toString());
+    };
+
+    getMassValue();
+  }, [recipeValue, volumeValue]);
+
+  return (
+    <div className="flex w-full max-w-lg flex-col items-start gap-8 rounded-md bg-zinc-800 py-4 px-16">
+      <div className="flex flex-col gap-2">
+        <p className="text-lg">What is the agar recipe?</p>
+        <div className="flex gap-2">
+          <input
+            type="number"
+            value={recipeValue}
+            onChange={handleRecipeChange}
+            className="w-full rounded-sm bg-zinc-100 px-2 text-2xl font-semibold text-black "
+          />
+          <span>g/mL</span>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="text-lg">What is the final volume you need?</p>
+        <div className="flex gap-2">
+          <input
+            type="number"
+            value={volumeValue}
+            onChange={handleVolumeChange}
+            className="w-full rounded-sm bg-zinc-100 px-2 text-2xl font-semibold text-black "
+          />
+          <span>mL</span>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="text-lg">What is the final volume you need?</p>
+        <div className="flex gap-2">
+          <input
+            readOnly
+            disabled
+            type="number"
+            value={massValue}
+            className="w-full rounded-sm bg-zinc-100 px-2 text-2xl font-semibold text-black "
+          />
+          <span>g</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
