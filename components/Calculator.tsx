05ab263233bdb80a663bcf86527bcfeebda52ce7
Javier <javierneri81@gmail.com>
2022-10-09 14:51:24 +0800

    style: move answer block into its own card

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -27,42 +27,46 @@ const Calculator = () => {
   }, [recipeValue, volumeValue]);
 
   return (
-    <div className="flex w-full max-w-lg flex-col items-start gap-8 rounded-md bg-zinc-800 py-4 px-16">
-      <div className="flex flex-col gap-2">
-        <p className="text-lg">What is the agar recipe?</p>
-        <div className="flex gap-2">
-          <input
-            type="number"
-            value={recipeValue}
-            onChange={handleRecipeChange}
-            className="w-full rounded-sm bg-zinc-100 px-2 text-2xl font-semibold text-black "
-          />
-          <span>g/L</span>
+    <div className="flex flex-col gap-8">
+      <div className="flex w-full max-w-lg flex-col items-start gap-8 rounded-md bg-zinc-800 py-4 px-16">
+        <div className="flex flex-col gap-2">
+          <p className="text-lg">What is the agar recipe?</p>
+          <div className="flex gap-2">
+            <input
+              type="number"
+              value={recipeValue}
+              onChange={handleRecipeChange}
+              className="w-full rounded-sm bg-zinc-100 px-2 text-2xl font-semibold text-black "
+            />
+            <span>g/L</span>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="text-lg">What is the final volume you need?</p>
+          <div className="flex gap-2">
+            <input
+              type="number"
+              value={volumeValue}
+              onChange={handleVolumeChange}
+              className="w-full rounded-sm bg-zinc-100 px-2 text-2xl font-semibold text-black "
+            />
+            <span>mL</span>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col gap-2">
-        <p className="text-lg">What is the final volume you need?</p>
-        <div className="flex gap-2">
-          <input
-            type="number"
-            value={volumeValue}
-            onChange={handleVolumeChange}
-            className="w-full rounded-sm bg-zinc-100 px-2 text-2xl font-semibold text-black "
-          />
-          <span>mL</span>
-        </div>
-      </div>
-      <div className="flex flex-col gap-2">
-        <p className="text-lg">You should put this amount of powder:</p>
-        <div className="flex gap-2">
-          <input
-            readOnly
-            disabled
-            type="number"
-            value={massValue}
-            className="w-full rounded-sm bg-zinc-100 px-2 text-2xl font-semibold text-black "
-          />
-          <span>g</span>
+      <div className="flex w-full max-w-lg flex-col items-start gap-8 rounded-md bg-zinc-800 py-4 px-16">
+        <div className="flex flex-col gap-2">
+          <p className="text-lg">You should put this amount of powder:</p>
+          <div className="flex gap-2">
+            <input
+              readOnly
+              disabled
+              type="number"
+              value={massValue}
+              className="w-full rounded-sm bg-zinc-300 px-2 text-2xl font-semibold text-black"
+            />
+            <span>g</span>
+          </div>
         </div>
       </div>
     </div>
