9bc1a6457495ba5a8fa0da8f8451a95f3c4e9e8c
Javier <javierneri81@gmail.com>
2022-10-10 03:05:47 +0800

    style: make more responsive
    Reduce x padding of card on small screens.
    
    Surround input with div to have consistent width.
    
    Change bg color of answer input to same color for UserInput.

--- components/AgarCalculator.tsx
+++ components/AgarCalculator.tsx
@@ -23,11 +23,13 @@ const AgarCalculator = () => {
     <div className="flex flex-col items-center gap-8">
       <h1 className="text-center text-3xl font-bold">Lab Calculator</h1>
 
-      <div className="flex w-full max-w-lg flex-col items-start gap-8 rounded-md bg-zinc-800 py-4 px-16">
+      <div className="flex w-full max-w-lg flex-col items-start gap-8 rounded-md bg-zinc-800 p-4 lg:px-16">
         <div className="flex flex-col gap-2">
           <p className="text-lg">What is the agar recipe?</p>
           <div className="flex gap-2">
-            <UserInput value={recipeValue} setValue={setRecipeValue} />
+            <div>
+              <UserInput value={recipeValue} setValue={setRecipeValue} />
+            </div>
             <span>g/L</span>
           </div>
         </div>
@@ -35,23 +37,27 @@ const AgarCalculator = () => {
         <div className="flex flex-col gap-2">
           <p className="text-lg">What is the final volume you need?</p>
           <div className="flex gap-2">
-            <UserInput value={volumeValue} setValue={setVolumeValue} />
+            <div>
+              <UserInput value={volumeValue} setValue={setVolumeValue} />
+            </div>
             <span>mL</span>
           </div>
         </div>
       </div>
 
-      <div className="flex w-full max-w-lg flex-col items-start gap-8 rounded-md bg-zinc-800 py-4 px-16">
+      <div className="flex w-full max-w-lg flex-col items-start gap-8 rounded-md bg-zinc-800 p-4 lg:px-16">
         <div className="flex flex-col gap-2">
           <p className="text-lg">You should put this amount of powder:</p>
           <div className="flex gap-2">
-            <input
-              readOnly
-              disabled
-              type="number"
-              value={massValue}
-              className="w-full rounded-sm bg-zinc-300 px-2 text-2xl font-semibold text-black"
-            />
+            <div>
+              <input
+                readOnly
+                disabled
+                type="number"
+                value={massValue}
+                className="w-full rounded-sm bg-zinc-100 px-2 text-2xl font-semibold text-black"
+              />
+            </div>
             <span>g</span>
           </div>
         </div>
