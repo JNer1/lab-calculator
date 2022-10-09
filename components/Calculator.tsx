aa65d9af4f3aad213c1df00d394f2f86c4511620
Javier <javierneri81@gmail.com>
2022-10-10 01:40:04 +0800

    chore: add space in code for readability
    Add line breaks to separate certain sections of the code to make it easier to read.

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -22,6 +22,7 @@ const Calculator = () => {
   return (
     <div className="flex flex-col items-center gap-8">
       <h1 className="text-center text-3xl font-bold">Lab Calculator</h1>
+
       <div className="flex w-full max-w-lg flex-col items-start gap-8 rounded-md bg-zinc-800 py-4 px-16">
         <div className="flex flex-col gap-2">
           <p className="text-lg">What is the agar recipe?</p>
@@ -30,6 +31,7 @@ const Calculator = () => {
             <span>g/L</span>
           </div>
         </div>
+
         <div className="flex flex-col gap-2">
           <p className="text-lg">What is the final volume you need?</p>
           <div className="flex gap-2">
@@ -38,6 +40,7 @@ const Calculator = () => {
           </div>
         </div>
       </div>
+
       <div className="flex w-full max-w-lg flex-col items-start gap-8 rounded-md bg-zinc-800 py-4 px-16">
         <div className="flex flex-col gap-2">
           <p className="text-lg">You should put this amount of powder:</p>
