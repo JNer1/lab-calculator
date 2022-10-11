e873c6d6cfa9f3486c730a643e23ed0d313e33ee
Javier <javierneri81@gmail.com>
2022-10-12 02:16:40 +0800

    feat: calculate final answer if isLiquid
    Add additional field if isLiquid asking for initial concentration.
    
    Create useEffect to get answer.

--- components/AntibioticCalculator.tsx
+++ components/AntibioticCalculator.tsx
@@ -2,9 +2,10 @@ import { ChangeEvent, useEffect, useState } from "react";
 import UserInput from "./UserInput";
 
 const AntibioticCalculator = () => {
-  const [recipeValue, setRecipeValue] = useState("0");
-  const [volumeValue, setVolumeValue] = useState("0");
-  const [massValue, setMassValue] = useState("0");
+  const [initialConcentration, setInitialConcentration] = useState("0");
+  const [finalConcentration, setFinalConcentration] = useState("0");
+  const [volume, setVolume] = useState("0");
+  const [answer, setAnswer] = useState("0");
 
   const [isLiquid, setIsLiquid] = useState(true);
   const [isPowder, setIsPowder] = useState(false);
@@ -35,18 +36,25 @@ const AntibioticCalculator = () => {
     setIsMicro(e.target.checked);
   };
 
+  const liquidFormula = (ci: number, cf: number, v: number) => {
+    const answer = (cf * v) / ci;
+
+    if (!answer) {
+      return 0;
+    }
+
+    return answer;
+  };
+
   useEffect(() => {
-    const concentration = parseFloat(recipeValue) || 0;
-    const finalvol = parseFloat(volumeValue) || 0;
+    const ci = parseFloat(initialConcentration);
+    const cf = parseFloat(finalConcentration) || 0;
+    const v = parseFloat(volume) || 0;
 
-    const getMassFormula = (concentration * finalvol) / 1000;
-
-    const getMassValue = () => {
-      setMassValue(getMassFormula.toFixed(2).toString());
-    };
-
-    getMassValue();
-  }, [recipeValue, volumeValue]);
+    if (isLiquid) {
+      setAnswer(liquidFormula(ci, cf, v).toFixed(2).toString());
+    }
+  }, [initialConcentration, finalConcentration, volume, isLiquid]);
 
   return (
     <div className="flex flex-col items-center gap-8">
@@ -105,28 +113,39 @@ const AntibioticCalculator = () => {
       </div>
 
       <div className="flex max-w-lg flex-col items-start gap-8 rounded-md bg-zinc-800 p-4 lg:px-16">
+        {isLiquid && (
+          <div className="flex flex-col gap-2">
+            <p className="text-lg">What is your starting concentration?</p>
+            <div className="grid w-full grid-cols-4 place-content-end gap-x-2">
+              <UserInput
+                width="col-span-2"
+                value={initialConcentration}
+                setValue={setInitialConcentration}
+              />
+              <span className="col-span-1 self-end">
+                {isUnits ? "units/mL" : "μg/mL"}
+              </span>
+            </div>
+          </div>
+        )}
+
         <div className="flex flex-col gap-2">
-          <p className="text-lg">What conentration do you need?</p>
+          <p className="text-lg">What concentration do you need?</p>
           <div className="grid w-full grid-cols-4 place-content-end gap-x-2">
             <UserInput
               width="col-span-2"
-              value={recipeValue}
-              setValue={setRecipeValue}
+              value={finalConcentration}
+              setValue={setFinalConcentration}
             />
             <span className="col-span-1 self-end">
               {isUnits ? "units/mL" : "μg/mL"}
             </span>
           </div>
         </div>
-
         <div className="flex flex-col gap-2">
           <p className="text-lg">What is the final volume you need?</p>
           <div className="grid w-full grid-cols-4 place-content-end gap-x-2">
-            <UserInput
-              width="col-span-2"
-              value={volumeValue}
-              setValue={setVolumeValue}
-            />
+            <UserInput width="col-span-2" value={volume} setValue={setVolume} />
             <span className="col-span-1 self-end">mL</span>
           </div>
         </div>
@@ -134,18 +153,20 @@ const AntibioticCalculator = () => {
 
       <div className="flex w-full max-w-lg flex-col items-start gap-8 rounded-md bg-zinc-800 p-4 lg:px-16">
         <div className="flex flex-col gap-2">
-          <p className="text-lg">You should put this amount of powder:</p>
+          <p className="text-lg">You should put this amount of antibiotic:</p>
           <div className="grid grid-cols-4 gap-x-2">
             <div className="col-span-2">
               <input
                 readOnly
                 disabled
                 type="number"
-                value={massValue}
+                value={answer}
                 className="w-full rounded-sm bg-zinc-100 px-2 text-right text-2xl font-semibold text-black"
               />
             </div>
-            <span className="col-span-1">g</span>
+            <span className="col-span-1 self-end">
+              {isLiquid ? "mL" : "mg"}
+            </span>
           </div>
         </div>
       </div>
