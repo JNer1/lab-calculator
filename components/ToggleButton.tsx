0baae97c694ca1517ca07e9ac7f9e2e1f56a242d
Javier <javierneri81@gmail.com>
2022-10-15 00:36:31 +0800

    fix(toggle button): no padding
    Button was too small on mobile because of no padding.
    
    Add padding.

--- components/ToggleButton.tsx
+++ components/ToggleButton.tsx
@@ -15,7 +15,7 @@ const ToggleButton = (props: ToggleButtonProps) => {
       onClick={onClick}
       className={`${
         active ? " bg-teal-200 text-lilac-900" : "bg-lilac-700"
-      } cursor-pointer rounded-md text-center font-semibold transition duration-[120ms] ease-in-out hover:bg-teal-200 hover:text-lilac-900`}
+      } cursor-pointer rounded-md p-1 text-center font-semibold transition duration-[120ms] ease-in-out hover:bg-teal-200 hover:text-lilac-900`}
       tabIndex={0}
       onKeyDown={onKeyDown}
     >
