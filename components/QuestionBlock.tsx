6775462121e803c2fc348dbdefe588bca56a0714
Javier <javierneri81@gmail.com>
2022-10-14 23:49:05 +0800

    feat(question block): add disabled prop
    Add disabled prop to QuestionBlock and UserInput.

--- components/QuestionBlock.tsx
+++ components/QuestionBlock.tsx
@@ -6,16 +6,22 @@ interface QuestionBlockProps {
   value: string;
   setValue: Dispatch<SetStateAction<string>>;
   unit: string;
+  disabled?: boolean;
 }
 
 const QuestionBlock = (props: QuestionBlockProps) => {
-  const { question, value, setValue, unit } = props;
+  const { question, value, setValue, unit, disabled } = props;
 
   return (
     <div className="flex flex-col gap-2">
       <p className="text-lg">{question}</p>
       <div className="grid w-full grid-cols-4 place-content-end gap-x-2">
-        <UserInput width="col-span-2" value={value} setValue={setValue} />
+        <UserInput
+          width="col-span-2"
+          value={value}
+          setValue={setValue}
+          disabled={disabled}
+        />
         <span className="col-span-1 self-end">{unit}</span>
       </div>
     </div>
--- components/UserInput.tsx
+++ components/UserInput.tsx
@@ -4,10 +4,11 @@ interface UserInputProps {
   value: string;
   setValue: Dispatch<SetStateAction<string>>;
   width?: string;
+  disabled?: boolean;
 }
 
 const UserInput = (props: UserInputProps) => {
-  const { value, setValue, width } = props;
+  const { value, setValue, width, disabled } = props;
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const regex = /^[0-9.]+$/;
@@ -38,7 +39,8 @@ const UserInput = (props: UserInputProps) => {
       onBlur={handleBlur}
       className={`${
         width ? width : "w-full"
-      } rounded-sm bg-zinc-100 px-2 text-right text-2xl font-semibold text-black`}
+      } rounded-sm bg-zinc-100 px-2 text-right text-2xl font-semibold text-black disabled:bg-zinc-400`}
+      disabled={disabled}
     />
   );
 };
