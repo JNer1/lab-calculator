0b73f4919cf5bc53dfe494de3e6db7aed390960a
Javier <javierneri81@gmail.com>
2023-02-04 00:46:58 +0800

    fix: use next font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Noto_Sans_Mono } from "@next/font/google";
+
+const notoSansMono = Noto_Sans_Mono({ subsets: ["latin", "greek"] });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={notoSansMono.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
