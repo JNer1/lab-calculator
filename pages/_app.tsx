75f531a9eefe0aa4d335e167b0888faaa1299b43
Javier <javierneri81@gmail.com>
2023-02-04 18:21:42 +0800

    fix: use next font variable for tailwind
    Implenent next font with tailwind as per the docs.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Noto_Sans_Mono } from "@next/font/google";
 
-const notoSansMono = Noto_Sans_Mono({ subsets: ["latin", "greek"] });
+const notoSansMono = Noto_Sans_Mono({
+  subsets: ["latin", "greek"],
+  variable: "--font-noto-sans-mono",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={notoSansMono.className}>
+    <div className={`${notoSansMono.variable} font-mono`}>
       <Component {...pageProps} />
     </div>
   );
