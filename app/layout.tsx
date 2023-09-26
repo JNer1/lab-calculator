9175b0c75e4c15deee0c74ee5e34d46377813c61
Javier Neri <javierneri81@gmail.com>
2023-09-26 22:31:26 +0800

    feat: add dialog to donate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Noto_Sans_Mono } from "next/font/google";
 import "../styles/globals.css";
 
+import DonationDialog from "../components/DonationDialog";
+
 const notoSansMono = Noto_Sans_Mono({
   subsets: ["latin", "greek"],
   variable: "--font-noto-sans-mono",
@@ -18,6 +20,10 @@ export default function RootLayout({
         className={`${notoSansMono.variable} bg-lilac-900 font-mono text-white`}
       >
         {children}
+        <footer className="flex flex-col items-center justify-center gap-4 border-t border-lilac-700 p-8">
+          <p>Because we all forget how to compute</p>
+          <DonationDialog />
+        </footer>
       </body>
     </html>
   );
