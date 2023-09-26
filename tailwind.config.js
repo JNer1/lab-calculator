7748af7a8fb9b158491eb8309f2e5b750d315878
Javier Neri <javierneri81@gmail.com>
2023-09-26 22:28:23 +0800

    feat: add more shaded of lilac from rose-pine

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
     extend: {
       colors: {
         lilac: {
+          100: "hsl(245deg, 50%, 91%)",
+          300: "hsl(248deg, 15%, 61%)",
+          500: "hsl(249deg, 15%, 28%)",
           700: "hsl(248deg, 25%, 18%)",
           900: "hsl(249, 22%, 12%)",
         },
